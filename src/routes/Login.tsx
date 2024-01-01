31fa567875b2fd10435d4bbae92d48705ddd1d25
Chr-K <104398517+Chr-K@users.noreply.github.com>
2024-01-01 08:28:45 -0800

    test

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -4,7 +4,7 @@ export default function Login(){
     const [formData,setFormData] = useState({username: '',password:''})
     async function handleSubmit(){
        await fetch('https://api.christopherkufis.com/articles',{
-            method:'POST',
+            method:'GET',
             mode:'cors',
             headers:{"content-type":"application/json"},
             redirect:"follow",
