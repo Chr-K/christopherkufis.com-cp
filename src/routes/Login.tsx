b7dbc94d368291fbed6d2db232c6da88bce67cea
Chr-K <104398517+Chr-K@users.noreply.github.com>
2024-01-01 12:02:45 -0800

    test

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -3,15 +3,22 @@ import { useState, ChangeEvent } from 'react'
 export default function Login(){
     const [formData,setFormData] = useState({username: '',password:''})
     async function handleSubmit(){
-       await fetch('https://api.christopherkufis.com/auth/',{
-            method:'POST',
-            mode:'cors',
-            headers:{"content-type":"application/json"},
-            redirect:"follow",
-            body:JSON.stringify(formData),
-        }).then((res)=>{
-            console.log(res.body)
-        })
+        try{
+            const res = await fetch('https://api.christopherkufis.com/auth/',{
+                method:'POST',
+                mode:'cors',
+                headers:{"content-type":"application/json"},
+                redirect:"follow",
+                body:JSON.stringify(formData),
+            })
+            if(res.ok){
+                console.log(res.body)
+            }
+        }
+        catch(err){
+
+        }
+
     }
 
     function handleInput(e: ChangeEvent<HTMLInputElement>){
