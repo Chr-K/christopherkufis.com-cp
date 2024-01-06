3bf819136e6c6f38cab365b21180ab0fc0e6da0e
Chr-K <104398517+Chr-K@users.noreply.github.com>
2024-01-06 12:25:30 -0800

    test

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,7 +1,6 @@
-import { Outlet, useNavigate } from "react-router-dom"
+import { Outlet } from "react-router-dom"
 import { Link } from "react-router-dom"
 export default function Root(){
-    const navigate = useNavigate()
     
    async function handleLogout(){
     try{
