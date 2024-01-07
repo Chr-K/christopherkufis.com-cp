be6da86c06b78367b5b15c56bae4468cb089c203
Chr-K <104398517+Chr-K@users.noreply.github.com>
2024-01-07 14:00:52 -0800

    test

--- src/components/ToolBar.tsx
+++ src/components/ToolBar.tsx
@@ -1,43 +1,52 @@
-import { useState } from 'react'
 import '../../styles/toolbar.css'
 export default function ToolBar(){
-    const [isBold,setIsBold] = useState("normal")
-    function Bold(){
+
+    function setTextStyle(cb:any){
         var selection = window.getSelection()
         var content = selection?.getRangeAt(0)
         var element = content?.startContainer.parentElement
-        //check if null
         if(element){
             if(element?.textContent){
                 if(content?.toString().length != element?.textContent.length){
                     const span = document.createElement('span')
-                    span.style.fontWeight = 'bold'
+                    cb(span)
                     content?.surroundContents(span)
-                    setIsBold('Bold')
                     window.getSelection()?.removeAllRanges()
-                    span.focus()
                 }
                 else{
-                    if(element.style.fontWeight != 'bold'){
-                        element.style.fontWeight = 'bold'
-                        setIsBold('Bold')
-                    }
-                    else{
-                        element.style.fontWeight = 'normal'
-                        setIsBold('normal')
-                    }
+                    cb(element)
                 }
             }
 
         }
-        //check if null
-
     }
-
+    function Bold(element:HTMLElement){
+        if(element.style.fontWeight != 'bold'){
+        element.style.fontWeight = 'bold'
+        }
+        else{
+            element.style.fontWeight = 'normal'
+        }
+    }
+    function Size(element:HTMLElement,selectedSize:string){
+        element.style.fontSize = selectedSize
+    }
 
     return(
         <div className="toolbar">
-            <button id='bold-btn' style={{fontWeight:isBold,fontSize:'large'}}  onClick={Bold}>B</button>
+            <button id='bold-btn' style={{fontSize:'large'}}  onClick={()=>{setTextStyle(Bold)}}>B</button>
+            <select id='size-select' className='size-btn' onChange={(e)=>{
+                const selectedSize = e.target.value
+                setTextStyle((element:HTMLElement) => Size(element,selectedSize))
+            }}>
+                <option className='size-option'></option>
+                <option className='size-option' value={'1rem'}>10</option>
+                <option className='size-option' value={'1.2rem'}>12</option>
+                <option className='size-option' value={'1.4rem'}>14</option>
+                <option className='size-option' value={'1.6rem'}>16</option>
+                <option className='size-option' value={'1.8rem'}>18</option>
+                <option className='size-option' value={'2.0rem'}>20</option>
+            </select>
         </div>
     )
 }
