162e78dc496bf7e96f4cc50286fa5cb41b062276
Chr-K <104398517+Chr-K@users.noreply.github.com>
2024-01-14 17:18:56 -0800

    test

--- src/components/articleSubmit.tsx
+++ src/components/articleSubmit.tsx
@@ -1,9 +1,34 @@
 import { useCurrentEditor } from "@tiptap/react"
+import { useNavigate } from "react-router-dom"
 
 export default function ArticleSubmit(){
     const {editor} = useCurrentEditor()
+    const navigate = useNavigate()
     async function submit(){
-        console.log(editor?.getHTML())
+        if(!editor){
+
+        }
+        else{
+            let title = document.getElementById("title-input") as HTMLInputElement
+            const res = await fetch('https://api.christopherkufis.com/submitarticle/',{
+                method:"POST",
+                mode:"cors",
+                credentials:'include',
+                headers:{
+                    'content-type':'application/json',
+                },
+                body:JSON.stringify({
+                    content:editor.getHTML,
+                    title:title.value
+                })
+            })   
+            if(res.ok){
+                res.json().then((res)=>{
+                    navigate('/edit-articles')
+                })
+            }
+        }
+
     }
     return(
     <>
